4b2752b3e92575332217de0de8ae271c85aadad0
verysvadym <veresivadim3@gmail.com>
2025-10-13 20:52:24 +0200

    changed authentication configuration

--- src/main/java/com/example/tickets/config/OpenApiConfig.java
+++ src/main/java/com/example/tickets/config/OpenApiConfig.java
@@ -2,18 +2,30 @@ package com.example.tickets.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().info(
-                new Info()
+        return new OpenAPI()
+                .info(new Info()
                         .title("Tickets API")
                         .version("1.0.0")
-                        .description("Semestrální projekt")
-        );
+                        .description("Semestrální projekt"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Zadejte JWT token ve formátu: Bearer <token>")
+                ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
+
